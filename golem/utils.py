83ff657200f690c82f181bfc0b305e24bd363e3a
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2014-06-29 17:34:33 +0200

    further refactoring of FunctionInspector

--- golem/utils.py
+++ golem/utils.py
@@ -19,24 +19,39 @@ class FunctionInspector(object):
         self._argspec = inspect.getargspec(func)
 
     def normalize(self, *args, **kwargs):
+        self.__validate(args, kwargs)
+        return self.__create_normalization_result(args, kwargs)
+
+    def __validate(self, args, kwargs):
+        given = len(args) + len(kwargs)
         for k in kwargs:
             if k not in self.arg_names:
                 raise TypeError("%s() got an unexpected keyword argument %r" % (self.func.func_name, k))
         if args and not self.arg_names:
             raise TypeError("%s() takes no arguments (%d given)" % (self.func.func_name, len(args)))
-        elif len(args) > len(self.arg_names):
-            raise TypeError(self.__render_invalid_number_of_args_message(args))
-        elif len(args) != len(self.arg_names_required):
-            raise TypeError(self.__render_invalid_number_of_args_message(args))
+        elif given > len(self.arg_names):
+            raise TypeError(self.__render_too_many_arguments_error(given))
+        elif given < len(self.arg_names_required):
+            raise TypeError(self.__render_too_few_arguments_error(given))
 
-    def __render_invalid_number_of_args_message(self, given):
+    def __create_normalization_result(self, args, kwargs):
+        result = dict(zip(self.arg_names_required, args))
+        result.update(self.arg_defaults)
+        result.update(kwargs)
+        return result
+
+    def __render_too_many_arguments_error(self, given):
         tmp = ["%s() takes" % self.func.func_name]
-        if self.min_args != self.max_args and len(given) > self.max_args:
-            tmp.append('at most %d' % self.max_args)
-        else:
-            tmp.append(('at least %d' if self.min_args != self.max_args else 'exactly %d') % self.min_args)
+        tmp.append(('at most %d' if self.min_args != self.max_args else 'exactly %d') % self.max_args)
         tmp.append('argument' if self.min_args == 1 else 'arguments')
-        tmp.append("(%d given)" % len(given))
+        tmp.append("(%d given)" % given)
+        return ' '.join(tmp)
+
+    def __render_too_few_arguments_error(self, given):
+        tmp = ["%s() takes" % self.func.func_name]
+        tmp.append(('at least %d' if self.min_args != self.max_args else 'exactly %d') % self.min_args)
+        tmp.append('argument' if self.min_args == 1 else 'arguments')
+        tmp.append("(%d given)" % given)
         return ' '.join(tmp)
 
     @property
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -29,10 +29,18 @@ class TestFunctionInspector(unittest.TestCase):
         with self.assertRaisesRegexp(TypeError, "foo\(\) takes no arguments \(1 given\)"):
             uut.normalize(1)
 
+    def test_ifFunctionTakesNoParametersAndCalledWithNoParameters_emptyDictIsReturned(self):
+        uut = FunctionInspector(self.foo)
+        self.assertEqual({}, uut.normalize())
+
     def test_ifFunctionDoesNotHaveArgumentOfGivenName_decodeRaisesTypeError(self):
         uut = FunctionInspector(self.bar)
         with self.assertRaisesRegexp(TypeError, "bar\(\) got an unexpected keyword argument 'b'"):
             uut.normalize(b=1)
+
+    def test_ifNormalizeCalledWithValidKeywordArgument_itIsReturnedAsResult(self):
+        uut = FunctionInspector(self.bar)
+        self.assertEqual({'a': 1}, uut.normalize(a=1))
 
     def test_ifDecodeCalledWithTooManyArguments_TypeErrorIsRaised(self):
         uut = FunctionInspector(self.bar)
@@ -42,9 +50,24 @@ class TestFunctionInspector(unittest.TestCase):
         with self.assertRaisesRegexp(TypeError, "baz\(\) takes exactly 2 arguments \(3 given\)"):
             uut.normalize(1, 2, 3)
 
+    def test_ifNormalizingPositionalArguments_theyAreDecodedToTheirNames(self):
+        uut = FunctionInspector(self.baz)
+        self.assertEqual({'a': 1, 'b': 2}, uut.normalize(1, 2))
+
+    def test_ifNormalizingPositionalAndKeywordArguments_theyAreDecodedToTheirNames(self):
+        uut = FunctionInspector(self.spam)
+        self.assertEqual({'a': 1, 'b': 2, 'c': 3}, uut.normalize(1, 2, c=3))
+
+    def test_ifNormalizingWithRequiredArgsOnly_defaultsAreIncludedInResultAsWell(self):
+        uut = FunctionInspector(self.spam)
+        self.assertEqual({'a': 1, 'b': 2, 'c': None}, uut.normalize(1, 2))
+
     def test_ifDecodeCalledWithTooFewArguments_TypeErrorIsRaised(self):
         uut = FunctionInspector(self.spam)
         with self.assertRaisesRegexp(TypeError, "spam\(\) takes at least 2 arguments \(1 given\)"):
+            uut.normalize(1)
+        uut = FunctionInspector(self.baz)
+        with self.assertRaisesRegexp(TypeError, "baz\(\) takes exactly 2 arguments \(1 given\)"):
             uut.normalize(1)
 
     def test_ifDecodeForFunctionWithDefaultArgsCalledWithTooManyArguments_TypeErrorIsRaised(self):
